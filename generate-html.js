a2cac1a87dd1bf9a92daeb32c84da25e8f57f797
pathanthanveerahmed <thanveerahmed@gmail.com>
2025-06-13 18:51:32 +0530

    Update generate-html.js

--- generate-html.js
+++ generate-html.js
@@ -1,19 +1,19 @@
 const fs = require('fs');
 
-// Load data.json
+// Load JSON data
 const data = JSON.parse(fs.readFileSync('dynamic/data.json', 'utf8'));
 
-// Get active sequence
+// Find active sequence
 const sequence = data.sequences.find(seq => seq[0] === data.active);
-const topId = sequence && sequence.length ? sequence[0] : data.images[0].id;
+const topId = sequence?.[0] || data.images[0].id;
 
-// Get top image data
+// Find top product details
 const topItem = data.images.find(img => img.id === topId) || {
   name: 'Newly Added Device',
   description: 'Latest certified pre-owned devices at SellInSeconds!',
 };
 
-// Generate image cards HTML
+// Build product cards HTML
 let cardsHTML = '';
 sequence.forEach(id => {
   const img = data.images.find(image => image.id === id) || {};
@@ -24,7 +24,7 @@ sequence.forEach(id => {
     <div class="card" id="product-${id}">
       <picture>
         <source srcset="/dynamic/images/${id}.webp" type="image/webp">
-        <img src="/dynamic/images/${id}.jpg" alt="${nameText}" loading="lazy">
+        <img src="/dynamic/images/${id}.webp" alt="${nameText}" loading="lazy">
       </picture>
       <div class="card-description">
         <div class="card-title">${nameText}</div>
@@ -34,17 +34,14 @@ sequence.forEach(id => {
   `;
 });
 
-// Load HTML template
+// Load and inject into template
 let template = fs.readFileSync('dynamic/buygallery-template.html', 'utf8');
-
-// Replace placeholders
 template = template
   .replace(/{{TITLE}}/g, topItem.name)
   .replace(/{{DESCRIPTION}}/g, topItem.description)
   .replace(/{{IMAGE}}/g, `${topId}.webp`)
   .replace(/{{CARDS}}/g, cardsHTML);
 
-// Write final file
+// Save final HTML
 fs.writeFileSync('dynamic/buygallery.html', template);
-
 console.log(`✅ buygallery.html generated with active ID ${topId}`);
