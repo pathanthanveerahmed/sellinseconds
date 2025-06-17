b8ac5767289153fbad626a72610402ff9a1b7ed7
pathanthanveerahmed <thanveerahmed@gmail.com>
2025-06-17 18:37:25 +0530

    Update generate-wacust.js

--- dynamic/generate-wacust.js
+++ dynamic/generate-wacust.js
@@ -11,7 +11,6 @@ for (let i = 1; i <= 30; i++) {
   const desc = item.description || "Certified device available";
   const filename = item.filename || "og.png";
 
-  // ✅ Avoid versioning in OG thumbnails
   const html = template
     .replace(/{{TITLE}}/g, title)
     .replace(/{{DESCRIPTION}}/g, desc)
