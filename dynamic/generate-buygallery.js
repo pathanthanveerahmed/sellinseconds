c026bea6cb88c25201367886280b069cc2c305ea
pathanthanveerahmed <thanveerahmed@gmail.com>
2026-05-17 18:57:49 +0530

    Update generate-buygallery.js

--- dynamic/generate-buygallery.js
+++ dynamic/generate-buygallery.js
@@ -2,7 +2,7 @@ const fs = require("fs");
 
 const dataPath = "dynamic/data.json";
 const templatePath = "dynamic/buygallery-template.html";
-const outputPath = "dynamic/deals.html";
+const outputPath = "dynamic/buygallery.html";
 
 // Step 1: Load data.json
 let data;
